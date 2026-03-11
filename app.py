f6232a27c6493511177c85223c5c94d6c3e0a0a0
Sidiq Abdulkadir Akanbi <sidiqabdulkadir05@gmail.com>
2026-03-11 01:15:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ submit_button = form.form_submit_button("Predict")
 
 
 if submit_button:
-input_data = [[fixed_acidity, volatile_acidity, citric_acid, residual_sugar,
+	input_data = [[fixed_acidity, volatile_acidity, citric_acid, residual_sugar,
                chlorides, free_sulfur_dioxide, total_sulfur_dioxide,
                density, pH, sulphates, alcohol]]
 	prediction = model.predict(input_data)
@@ -127,6 +127,7 @@ input_data = [[fixed_acidity, volatile_acidity, citric_acid, residual_sugar,
 	st.subheader("Prediction Result")
 
 	st.success(f" Predicted score: {prediction[0]}")
+
 
 
 
